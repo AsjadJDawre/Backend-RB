56a16d917121a893a1b73e6cff2aef690ee6b30f
Asjad Johar Dawre <dawreasjad72@gmail.com>
2025-02-10 09:50:30 +0530

    debug

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -6,7 +6,7 @@ import { User } from "../models/user.model.js";
 export const verifyJWT = asyncHandler(async (req, res, next) => {
 
 try {
-    console.log("i am in verify jwt")
+    console.log("i am in authenticate jwt")
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
     console.log(token)
     if (!token) {
